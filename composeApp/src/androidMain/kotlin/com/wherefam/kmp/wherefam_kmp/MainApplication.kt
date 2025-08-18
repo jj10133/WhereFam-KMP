724021579457d2e289912a8590bbff7a6ee92a76
JJ <warmachinesocial@skiff.com>
2025-08-18 18:51:10 -0500

    Integrated koin on android side

---
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/MainApplication.kt
@@ -0,0 +1,17 @@
+package com.wherefam.kmp.wherefam_kmp
+
+import android.app.Application
+import com.wherefam.kmp.wherefam_kmp.di.androidModule
+import com.wherefam.kmp.wherefam_kmp.di.sharedModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
+
+class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(androidModule, sharedModule)
+        }
+    }
+}
