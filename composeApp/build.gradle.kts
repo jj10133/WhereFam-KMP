a088e463f3a4bd9e371374b368e21805ee9927fa
JJ <warmachinesocial@skiff.com>
2025-08-18 18:50:37 -0500

    Added Koin as dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -35,6 +35,9 @@ kotlin {
             implementation(libs.androidx.activity.compose)
             api(fileTree(mapOf("dir" to "libs", "include" to listOf("bare-kit/classes.jar"))))
 
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
+
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -45,6 +48,10 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            api(libs.koin.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
