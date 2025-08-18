330265edf7d4c3244817528dac2d91df800110ab
JJ <warmachinesocial@skiff.com>
2025-08-18 16:33:05 -0500

    Fix - Integrated worklet on android side

--- composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/MainActivity.kt
@@ -4,22 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import to.holepunch.bare.kit.Worklet
 
 class MainActivity : ComponentActivity() {
+
+    private var worklet: Worklet? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        actionBar?.hide()
+
+        worklet = Worklet(null)
+
+        try {
+            worklet!!.start("/app.bundle", assets.open("app.bundle"), null)
+        } catch (e: Exception) {
+            throw RuntimeException( e)
+        }
 
         setContent {
             App()
         }
     }
-}
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
+    override fun onPause() {
+        super.onPause()
+        worklet!!.suspend()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        worklet!!.resume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        worklet!!.terminate()
+        worklet = null
+    }
 }
