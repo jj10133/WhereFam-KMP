ca8991ea4155f148e3b741159411840b17c545c2
JJ <warmachinesocial@skiff.com>
2025-08-19 01:00:53 -0500

    Initialized GenericMessageProcessor in Koin

--- composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/di/SharedModule.kt
+++ composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/di/SharedModule.kt
@@ -1,7 +1,10 @@
 package com.wherefam.kmp.wherefam_kmp.di
 
+import com.wherefam.kmp.wherefam_kmp.processing.GenericMessageProcessor
 import org.koin.dsl.module
 
 val sharedModule = module {
-
+    single {
+        GenericMessageProcessor()
+    }
 }
