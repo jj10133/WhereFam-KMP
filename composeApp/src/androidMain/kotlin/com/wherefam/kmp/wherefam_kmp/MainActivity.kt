ba5e537bcc7d52110fdade0eb8c466993f16e012
JJ <warmachinesocial@skiff.com>
2025-08-19 01:05:54 -0500

    Integrated IPC within MainActivity.kt to read messages

--- composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
+import com.wherefam.kmp.wherefam_kmp.data.IPCMessageConsumer
+import com.wherefam.kmp.wherefam_kmp.processing.GenericMessageProcessor
+import org.koin.android.ext.android.inject
+import to.holepunch.bare.kit.IPC
 import to.holepunch.bare.kit.Worklet
 
 class MainActivity : ComponentActivity() {
 
     private var worklet: Worklet? = null
+    private var ipc: IPC? = null
+    private val messageProcessor: GenericMessageProcessor by inject()
+    private var ipcMessageConsumer: IPCMessageConsumer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
@@ -19,6 +27,13 @@ class MainActivity : ComponentActivity() {
 
         try {
             worklet!!.start("/app.bundle", assets.open("app.bundle"), null)
+            ipc = IPC(worklet)
+
+            ipcMessageConsumer = IPCMessageConsumer(ipc!!, messageProcessor)
+            ipcMessageConsumer?.lifecycleScope = lifecycleScope
+            ipcMessageConsumer?.startConsuming()
+
+
         } catch (e: Exception) {
             throw RuntimeException( e)
         }
