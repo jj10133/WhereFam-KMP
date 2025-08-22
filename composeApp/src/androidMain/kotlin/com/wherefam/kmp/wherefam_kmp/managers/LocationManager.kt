0c0133767e04ad4d7b55fef5c61df6e73e24d9ef
JJ <warmachinesocial@skiff.com>
2025-08-22 18:49:57 -0500

    Added LocationManager.kt

---
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/managers/LocationManager.kt
@@ -0,0 +1,65 @@
+package com.wherefam.kmp.wherefam_kmp.managers
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import android.os.Looper
+import androidx.annotation.RequiresApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executors
+
+@SuppressLint("MissingPermission")
+class LocationManager(
+    context: Context
+) {
+
+    private val androidLocationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    fun getLocation(
+        onSuccess: (latitude: Double, longitude: Double) -> Unit,
+    ){
+        val executor = Executors.newSingleThreadExecutor()
+        androidLocationManager.getCurrentLocation(
+            LocationManager.GPS_PROVIDER,
+            null,
+            executor
+        ) { location ->
+            if (location != null) {
+                onSuccess(location.latitude, location.longitude)
+            }
+        }
+    }
+
+    fun trackLocation(): Flow<Location> {
+        return callbackFlow {
+            val locationListener = LocationListener { location ->
+                launch {
+                    send(location)
+                }
+            }
+
+            val updateInterval = 5000L
+            val minDistance = 10f
+
+            androidLocationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                updateInterval,
+                minDistance,
+                locationListener,
+                Looper.getMainLooper()
+            )
+
+            awaitClose {
+                androidLocationManager.removeUpdates(locationListener)
+            }
+        }
+    }
+}
