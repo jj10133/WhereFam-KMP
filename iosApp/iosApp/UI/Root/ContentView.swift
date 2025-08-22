44563845050ed3f979ec7888b89495c7a653db97
JJ <warmachinesocial@skiff.com>
2025-08-22 18:53:04 -0500

    Fix - Moved ContentView.swift and updated to use onboarding views

---
+++ iosApp/iosApp/UI/Root/ContentView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import ConcentricOnboarding
+
+//struct ComposeView: UIViewControllerRepresentable {
+//    func makeUIViewController(context: Context) -> UIViewController {
+//        MainViewControllerKt.MainViewController()
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+//    }
+//}
+
+struct ContentView: View {
+    @AppStorage("completedOnboarding") var completedOnboarding = false
+    
+    var body: some View {
+       Group {
+           if (completedOnboarding) {
+               HomeView()
+           } else {
+               ConcentricOnboardingView(pageContents:
+                                           [ (AnyView(FirstPageView()), Color(UIColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))),
+                                             (AnyView(SecondPageView()), Color(UIColor(#colorLiteral(red: 1, green: 0.7349339692, blue: 0.5137254902, alpha: 1)))),
+                                             (AnyView(ThirdPageView()), Color(UIColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))),
+                                             (AnyView(FourthPageView()), Color(UIColor(#colorLiteral(red: 1, green: 0.7349339692, blue: 0.5137254902, alpha: 1)))),
+                                             (AnyView(SixthPageView()), Color(UIColor(#colorLiteral(red: 1, green: 0.7333333333, blue: 0.5137254902, alpha: 1))))
+                                           ])
+               .nextIcon("chevron.right")
+               .didGoToLastPage {
+                   DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                       completedOnboarding = true
+                   }
+               }
+           }
+       }
+    }
+}
