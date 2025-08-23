19c3ff7464a567d39bae6491dbc88947ea019e39
JJ <warmachinesocial@skiff.com>
2025-08-22 22:37:51 -0500

    Added ProvideFeedbackView.kt

---
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/ui/home/ProvideFeedbackView.kt
@@ -0,0 +1,10 @@
+package com.wherefam.kmp.wherefam_kmp.ui.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun ProvideFeedbackView() {
+    Text("Provide Feedback Content")
+}
