7af38418749ec79261f955ceb80e4f5171a10829
JJ <warmachinesocial@skiff.com>
2025-08-23 18:53:26 -0500

    Fix - Deleted it!!

--- composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.wherefam.kmp.wherefam_kmp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.wherefam.kmp.wherefam_kmp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
