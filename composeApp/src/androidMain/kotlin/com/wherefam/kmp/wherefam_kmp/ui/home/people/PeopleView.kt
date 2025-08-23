345e3bc695ad2e10f34175a9e848817103768635
JJ <warmachinesocial@skiff.com>
2025-08-22 22:37:25 -0500

    Added PeopleView.kt

---
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/ui/home/people/PeopleView.kt
@@ -0,0 +1,170 @@
+package com.wherefam.kmp.wherefam_kmp.ui.home.people
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun PeopleView(peopleViewModel: PeopleViewModel = koinViewModel()) {
+    val peopleList by peopleViewModel.peopleList.collectAsState()
+    var showDialog by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "People",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.weight(1f)
+            )
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add",
+                modifier = Modifier
+                    .size(30.dp)
+                    .clickable {
+                        showDialog = true
+                    }
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(MaterialTheme.colorScheme.surfaceVariant)
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                items(peopleList, key = { it.id }) { person ->
+                    SwipeToDeleteContainer(
+                        item = person,
+                        onDelete = {
+                            peopleViewModel.removePerson(person.id)
+                        }
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(MaterialTheme.colorScheme.surfaceVariant)
+                                .padding(16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "Contact ${person.id} - ${person.name}",
+                                fontSize = 18.sp,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        }
+                    }
+
+                    if (person != peopleList.lastOrNull()) {
+                        HorizontalDivider(modifier = Modifier.padding(horizontal = 8.dp))
+                    }
+                }
+            }
+        }
+
+    }
+
+    if (showDialog) {
+        AddPeopleDialog(
+            onConfirm = { newPersonId ->
+                peopleViewModel.addPerson(Peer(newPersonId, null, null, null))
+                coroutineScope.launch {
+                    peopleViewModel.joinPeer(newPersonId)
+                }
+                showDialog = false
+            },
+            onDismiss = {
+                showDialog = false
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddPeopleDialog(onConfirm: (String) -> Unit, onDismiss: () -> Unit) {
+    var dialogInput by remember { mutableStateOf("") }
+
+    BasicAlertDialog(
+        onDismissRequest = onDismiss,
+    ) {
+        Surface(
+            modifier = Modifier
+                .wrapContentWidth()
+                .wrapContentHeight(),
+            tonalElevation = AlertDialogDefaults.TonalElevation,
+            shape = MaterialTheme.shapes.large
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    "Add People",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(8.dp)
+                )
+
+                OutlinedTextField(
+                    value = dialogInput,
+                    onValueChange = { dialogInput = it },
+                    label = { Text("Enter ID") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    TextButton(
+                        onClick = onDismiss,
+                    ) {
+                        Text("Cancel")
+                    }
+
+                    TextButton(
+                        onClick = {
+                            onConfirm(dialogInput)
+                            dialogInput = ""
+                        }
+                    ) {
+                        Text("OK")
+                    }
+                }
+            }
+        }
+    }
+}
