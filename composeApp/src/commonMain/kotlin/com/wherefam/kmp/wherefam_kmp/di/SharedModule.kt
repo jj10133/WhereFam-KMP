bcebc57272faa96aff3bf44f37b05ceddb7f0af6
JJ <warmachinesocial@skiff.com>
2025-08-23 22:53:09 -0500

    Fix - Refactored to pass the dao rather than db instance

--- composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/di/SharedModule.kt
+++ composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/di/SharedModule.kt
@@ -1,5 +1,6 @@
 package com.wherefam.kmp.wherefam_kmp.di
 
+import com.wherefam.kmp.wherefam_kmp.data.WhereFamDatabase
 import com.wherefam.kmp.wherefam_kmp.data.getRoomDatabase
 import com.wherefam.kmp.wherefam_kmp.processing.GenericMessageProcessor
 import org.koin.core.module.Module
@@ -8,10 +9,7 @@ import org.koin.dsl.module
 
 expect val targetModule: Module
 val sharedModule = module {
-    single {
-        GenericMessageProcessor()
-    }
-
-    single { getRoomDatabase(get()) }
-
+    single { getRoomDatabase(get()) as WhereFamDatabase }
+    single { get<WhereFamDatabase>().peerDao() }
+    single { GenericMessageProcessor(get(), get()) }
 }
