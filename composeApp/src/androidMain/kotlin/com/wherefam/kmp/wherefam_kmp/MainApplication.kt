cd1f82eeb7715f045fe06b7e7e7f5d033b2fe3cd
JJ <warmachinesocial@skiff.com>
2025-09-03 14:45:50 -0500

    Fix - Added android viewmodel from koin

--- composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/MainApplication.kt
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/MainApplication.kt
@@ -2,18 +2,17 @@ package com.wherefam.kmp.wherefam_kmp
 
 import android.app.Application
 import com.wherefam.kmp.wherefam_kmp.di.androidModule
-import com.wherefam.kmp.wherefam_kmp.di.sharedModule
-import com.wherefam.kmp.wherefam_kmp.di.targetModule
-import com.wherefam.kmp.wherefam_kmp.di.viewModelModule
+import com.wherefam.kmp.wherefam_kmp.di.androidViewModelModule
+import com.wherefam.kmp.wherefam_kmp.di.initKoin
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.component.KoinComponent
 
-class MainApplication: Application() {
+class MainApplication: Application(), KoinComponent {
     override fun onCreate() {
         super.onCreate()
-        startKoin {
+        initKoin {
             androidContext(this@MainApplication)
-            modules(androidModule, viewModelModule, targetModule, sharedModule)
+            modules(androidModule, androidViewModelModule)
         }
     }
 }
