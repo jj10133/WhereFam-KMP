e70eeeb1ddc1a2ad73ed7b1d6ab3eb65da9f3970
JJ <warmachinesocial@skiff.com>
2025-09-03 14:48:32 -0500

    Fix - using stateflow

--- composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/viewmodel/PeopleViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/viewmodel/PeopleViewModel.kt
@@ -1,22 +1,26 @@
-package com.wherefam.kmp.wherefam_kmp.ui.home.people
+package com.wherefam.kmp.wherefam_kmp.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.wherefam.kmp.wherefam_kmp.data.IPCUtils.writeAsync
-import com.wherefam.kmp.wherefam_kmp.domain.Peer
+import com.wherefam.kmp.wherefam_kmp.data.IpcManager
+import com.wherefam.kmp.wherefam_kmp.model.Peer
 import com.wherefam.kmp.wherefam_kmp.domain.PeerRepository
 import com.wherefam.kmp.wherefam_kmp.processing.GenericAction
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
-import to.holepunch.bare.kit.IPC
-import java.nio.ByteBuffer
-import java.nio.charset.Charset
 
-class PeopleViewModel(private val peerRepository: PeerRepository, private val ipc: IPC) : ViewModel() {
-    val peersList: Flow<List<Peer>> = peerRepository.getAllPeers()
+class PeopleViewModel(private val peerRepository: PeerRepository, private val ipcManager: IpcManager) : ViewModel() {
+    val peersList: StateFlow<List<Peer>> = peerRepository.getAllPeers()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
 
     fun addPeer(id: String, name: String, lat: Double, lng: Double) {
         viewModelScope.launch {
@@ -37,8 +41,7 @@ class PeopleViewModel(private val peerRepository: PeerRepository, private val ip
             }
             val message = GenericAction(action = "joinPeer", data = dynamicData)
             val jsonString = Json.Default.encodeToString(message) + "\n"
-            val byteBuffer = ByteBuffer.wrap(jsonString.toByteArray(Charset.forName("UTF-8")))
-            ipc.writeAsync(byteBuffer)
+            ipcManager.write(jsonString)
         }
     }
 
