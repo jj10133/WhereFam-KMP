d5aa8fbf8e46eb121a8e269e8e647cb014608aa5
JJ <warmachinesocial@skiff.com>
2025-09-03 14:46:33 -0500

    Fix - Use of db using koin

--- composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/domain/PeerRepository.kt
+++ composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/domain/PeerRepository.kt
@@ -1,16 +1,20 @@
 package com.wherefam.kmp.wherefam_kmp.domain
 
-import com.wherefam.kmp.wherefam_kmp.data.dao.PeerDao
+import com.wherefam.kmp.wherefam_kmp.database.WhereFamDatabase
+import com.wherefam.kmp.wherefam_kmp.model.Peer
 import kotlinx.coroutines.flow.Flow
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class PeerRepository(private val peerDao: PeerDao) {
-    fun getAllPeers(): Flow<List<Peer>> = peerDao.getAllPeers()
+class PeerRepository() : KoinComponent {
+    private val database: WhereFamDatabase by inject()
+    fun getAllPeers(): Flow<List<Peer>> = database.peerDao().getAllPeers()
 
     suspend fun upsert(id: String, name: String, latitude: Double, longitude: Double) {
-        peerDao.upsert(Peer(id, name, latitude, longitude))
+        database.peerDao().upsert(Peer(id, name, latitude, longitude))
     }
 
     suspend fun deleteById(id: String) {
-        peerDao.deleteById(id)
+        database.peerDao().deleteById(id)
     }
 }
