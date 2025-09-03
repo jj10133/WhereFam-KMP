dfac53551047efb7c1f4217b56664e8d655010e9
JJ <warmachinesocial@skiff.com>
2025-09-03 14:40:53 -0500

    Fix - Refactored DI

--- composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/di/AndroidModule.kt
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/di/AndroidModule.kt
@@ -1,34 +1,25 @@
 package com.wherefam.kmp.wherefam_kmp.di
 
 import com.wherefam.kmp.wherefam_kmp.data.DataStoreRepository
-import com.wherefam.kmp.wherefam_kmp.data.IPCProvider
-import com.wherefam.kmp.wherefam_kmp.database.getDatabaseBuilder
+import com.wherefam.kmp.wherefam_kmp.data.IpcManager
 import com.wherefam.kmp.wherefam_kmp.managers.LocationManager
-import com.wherefam.kmp.wherefam_kmp.ui.home.HomeViewModel
-import com.wherefam.kmp.wherefam_kmp.ui.home.people.PeopleViewModel
+import com.wherefam.kmp.wherefam_kmp.viewmodel.HomeViewModel
+import com.wherefam.kmp.wherefam_kmp.viewmodel.PeopleViewModel
 import com.wherefam.kmp.wherefam_kmp.ui.home.share.ShareViewModel
 import com.wherefam.kmp.wherefam_kmp.ui.onboarding.OnboardingViewModel
 import com.wherefam.kmp.wherefam_kmp.ui.onboarding.SplashViewModel
 import com.wherefam.kmp.wherefam_kmp.ui.onboarding.ThirdPageViewModel
-import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val androidModule = module {
     single { LocationManager(get()) }
-    single { IPCProvider.ipc }
     single { DataStoreRepository(get()) }
 }
 
-val viewModelModule = module {
+val androidViewModelModule = module {
     viewModel { SplashViewModel(get()) }
     viewModel { OnboardingViewModel(get()) }
     viewModel { ThirdPageViewModel(get(), get()) }
-    viewModel { HomeViewModel(get(), get(), get()) }
     viewModel { ShareViewModel(get(), get()) }
-    viewModel { PeopleViewModel(get(), get()) }
-}
-
-actual val targetModule: Module = module {
-    single { getDatabaseBuilder(get()) }
 }
--- composeApp/src/iosMain/kotlin/com/wherefam/kmp/wherefam_kmp/di/iosModule.kt
+++ composeApp/src/iosMain/kotlin/com/wherefam/kmp/wherefam_kmp/di/iosModule.kt
@@ -1,12 +1,6 @@
 package com.wherefam.kmp.wherefam_kmp.di
 
-import com.wherefam.kmp.wherefam_kmp.database.getDatabaseBuilder
-import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val iosModule = module {
-}
-
-actual val targetModule: Module = module {
-    single { getDatabaseBuilder() }
 }
