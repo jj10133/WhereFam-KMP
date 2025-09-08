d92c2f50e39719ab0ec2639430f427197825b927
JJ <warmachinesocial@skiff.com>
2025-09-08 12:24:52 -0500

    Fix - Using Fused client rather than relying only on GPS

--- composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/managers/LocationManager.kt
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/managers/LocationManager.kt
@@ -8,6 +8,10 @@ import android.location.LocationManager
 import android.os.Build
 import android.os.Looper
 import androidx.annotation.RequiresApi
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -19,45 +23,86 @@ class LocationManager(
     context: Context
 ) {
 
-    private val androidLocationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//    private val androidLocationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
 
     fun getLocation(
         onSuccess: (latitude: Double, longitude: Double) -> Unit,
     ){
-        val executor = Executors.newSingleThreadExecutor()
-        androidLocationManager.getCurrentLocation(
-            LocationManager.GPS_PROVIDER,
-            null,
-            executor
-        ) { location ->
-            if (location != null) {
+//        val executor = Executors.newSingleThreadExecutor()
+//        androidLocationManager.getCurrentLocation(
+//            LocationManager.GPS_PROVIDER,
+//            null,
+//            executor
+//        ) { location ->
+//            if (location != null) {
+//                onSuccess(location.latitude, location.longitude)
+//            }
+//        }
+
+        fusedLocationClient
+            .lastLocation
+            .addOnSuccessListener { location ->
                 onSuccess(location.latitude, location.longitude)
             }
-        }
     }
 
     fun trackLocation(): Flow<Location> {
         return callbackFlow {
-            val locationListener = LocationListener { location ->
+//            val locationListener = LocationListener { location ->
+//                launch {
+//                    send(location)
+//                }
+//            }
+//
+//            val updateInterval = 2000L
+//            val minDistance = 10f
+//
+//            androidLocationManager.requestLocationUpdates(
+//                LocationManager.GPS_PROVIDER,
+//                updateInterval,
+//                minDistance,
+//                locationListener,
+//                Looper.getMainLooper()
+//            )
+//
+//            awaitClose {
+//                androidLocationManager.removeUpdates(locationListener)
+//            }
+
+            val locationCallback = locationCallback { location ->
                 launch {
                     send(location)
                 }
             }
 
-            val updateInterval = 2000L
-            val minDistance = 10f
+            val request = LocationRequest
+                .Builder(1000)
+                .build()
 
-            androidLocationManager.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER,
-                updateInterval,
-                minDistance,
-                locationListener,
+            fusedLocationClient.requestLocationUpdates(
+                request,
+                locationCallback,
                 Looper.getMainLooper()
             )
 
             awaitClose {
-                androidLocationManager.removeUpdates(locationListener)
+                fusedLocationClient.removeLocationUpdates(locationCallback)
+            }
+        }
+    }
+
+    private fun locationCallback(
+        onResult: (location: Location) -> Unit,
+    ): LocationCallback {
+        return object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult) {
+                super.onLocationResult(result)
+
+                result.lastLocation?.let { location ->
+                    onResult(location)
+                }
             }
         }
     }
