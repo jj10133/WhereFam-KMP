daeb368e406214dff6b5b32f68df7747960b2f98
JJ <warmachinesocial@skiff.com>
2025-09-20 15:51:28 -0500

    Added OnboardingStatus.kt

---
+++ composeApp/src/commonMain/kotlin/com/wherefam/kmp/wherefam_kmp/database/OnboardingStatus.kt
@@ -0,0 +1,10 @@
+package com.wherefam.kmp.wherefam_kmp.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "onboarding_status")
+data class OnboardingStatus(
+    @PrimaryKey val key: String = "onboarding_completed",
+    val value: Boolean
+)
