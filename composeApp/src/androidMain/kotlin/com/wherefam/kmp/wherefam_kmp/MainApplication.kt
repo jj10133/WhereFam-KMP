664af3c896492df258a0507f3fd2b93419107dd9
JJ <warmachinesocial@skiff.com>
2025-09-20 19:48:22 -0500

    Fix - Initialized revenuecat

--- composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/MainApplication.kt
+++ composeApp/src/androidMain/kotlin/com/wherefam/kmp/wherefam_kmp/MainApplication.kt
@@ -1,6 +1,9 @@
 package com.wherefam.kmp.wherefam_kmp
 
 import android.app.Application
+import com.revenuecat.purchases.LogLevel
+import com.revenuecat.purchases.Purchases
+import com.revenuecat.purchases.PurchasesConfiguration
 import com.wherefam.kmp.wherefam_kmp.di.androidModule
 import com.wherefam.kmp.wherefam_kmp.di.androidViewModelModule
 import com.wherefam.kmp.wherefam_kmp.di.initKoin
@@ -12,6 +15,12 @@ class MainApplication: Application(), KoinComponent {
     override fun onCreate() {
         super.onCreate()
         MapLibre.getInstance(this)
+        Purchases.logLevel = LogLevel.DEBUG
+        Purchases.configure(
+            PurchasesConfiguration
+                .Builder(this, "goog_FqCrHWFWPZKPTswBIDviqGgazAH")
+                .build()
+        )
 
         initKoin {
             androidContext(this@MainApplication)
